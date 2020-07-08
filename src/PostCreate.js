c28e231eb3208081c8abec0a45d912f7a8824136
Lyss <lyssfitz@outlook.com>
2020-07-08 19:05:46 +1000

    Add PostList component

--- src/PostCreate.js
+++ src/PostCreate.js
@@ -19,7 +19,7 @@ export default () => {
 			<form onSubmit={onSubmit}>
 				<div className='form-group'>
 					<label>Title</label>
-					<input value={title} onChange={(e) => e.target.value} className='form-control' />
+					<input value={title} onChange={(e) => setTitle(e.target.value)} className='form-control' />
 				</div>
 				<button className='btn btn-primary'>Submit</button>
 			</form>
